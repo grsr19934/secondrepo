b1da4fcb65a669e4987ea6fadbacad2811f1686b
demo <grsr1993@gmail.com>
2019-03-09 17:20:54 -0600

    First commit

---
+++ Hibernate1/src/main/java/com/example/Hibernate1/Hibernate1Application.java
@@ -0,0 +1,14 @@
+package com.example.Hibernate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate1Application.class, args);
+	}
+
+}
+
---
+++ Hibernate1/src/test/java/com/example/Hibernate1/Hibernate1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.Hibernate1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hibernate1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
